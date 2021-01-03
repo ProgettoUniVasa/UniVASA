4fdbf5e0b0381c4665e5983aa391332cf20f8dd6
MarcoChiavaroli99 <65245714+MarcoChiavaroli99@users.noreply.github.com>
2021-01-03 17:47:51 +0100

    domain

--- src/domain/Votazione.java
+++ src/domain/Votazione.java
@@ -1,8 +1,14 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Votazione {
 
 	private Long id;
+	private Elettore elettore;
+	private Evento evento;
+	private List<Elettore> preferenzeEspresse = new ArrayList<>();
 
 	public Long getId() {
 		return id;
@@ -11,5 +17,30 @@ public class Votazione {
 	public void setId(Long id) {
 		this.id = id;
 	}
+
+	public Elettore getElettore() {
+		return elettore;
+	}
+
+	public void setElettore(Elettore elettore) {
+		this.elettore = elettore;
+	}
+
+	public Evento getEvento() {
+		return evento;
+	}
+
+	public void setEvento(Evento evento) {
+		this.evento = evento;
+	}
+
+	public List<Elettore> getPreferenzeEspresse() {
+		return preferenzeEspresse;
+	}
+
+	public void setPreferenzeEspresse(List<Elettore> preferenzeEspresse) {
+		this.preferenzeEspresse = preferenzeEspresse;
+	}
+	
 	
 }
