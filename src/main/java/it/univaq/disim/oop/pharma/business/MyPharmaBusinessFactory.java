b6d9e4dc5d72defc4dde2c1f95f8726ad257ad00
Martina99188 <martina_nolletti@yahoo.it>
2021-01-27 17:45:07 +0100

    ..

--- src/main/java/it/univaq/disim/oop/pharma/business/MyPharmaBusinessFactory.java
+++ src/main/java/it/univaq/disim/oop/pharma/business/MyPharmaBusinessFactory.java
@@ -1,12 +1,10 @@
 package it.univaq.disim.oop.pharma.business;
 
 import it.univaq.disim.oop.pharma.business.impl.db.DbMyPharmaBusinessFactoryImpl;
-import it.univaq.disim.oop.pharma.business.impl.file.FileMyPharmaBusinessFactoryImpl;
 
 public abstract class MyPharmaBusinessFactory {
 
-	private static MyPharmaBusinessFactory factory = new FileMyPharmaBusinessFactoryImpl();
-	//private static MyPharmaBusinessFactory factory = new DbMyPharmaBusinessFactoryImpl();
+	private static MyPharmaBusinessFactory factory = new DbMyPharmaBusinessFactoryImpl();
 
 	public static MyPharmaBusinessFactory getInstance() {
 		return factory;
