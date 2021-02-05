d8bad5c031ee803ff5083a084b4996d987e8cdce
enrico.adamelli <enricosimone.adamelli@student.univaq.it>
2021-02-05 18:28:24 +0100

    votazione online

---
+++ src/main/java/it/univaq/disim/ing/univasa/controller/elettorecontroller/VotazioneOnlineController.java
@@ -0,0 +1,53 @@
+package it.univaq.disim.ing.univasa.controller.elettorecontroller;
+
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import it.univaq.disim.ing.univasa.business.BusinessException;
+import it.univaq.disim.ing.univasa.business.EventoService;
+import it.univaq.disim.ing.univasa.business.UnivasaBusinessFactory;
+import it.univaq.disim.ing.univasa.controller.DataInitializable;
+import it.univaq.disim.ing.univasa.domain.Candidato;
+import it.univaq.disim.ing.univasa.domain.Elettore;
+import it.univaq.disim.ing.univasa.domain.Evento;
+import it.univaq.disim.ing.univasa.view.ViewDispatcher;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+public class VotazioneOnlineController implements Initializable, DataInitializable<Evento> {
+	private ViewDispatcher dispatcher;
+
+	private EventoService eventoService;
+	
+	private Elettore elettore;
+
+	public VotazioneOnlineController() {
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		dispatcher = ViewDispatcher.getInstance();
+		UnivasaBusinessFactory factory = UnivasaBusinessFactory.getInstance();
+		eventoService = factory.getEventoService();
+	}
+	
+	@Override
+	public void initializeData(Evento evento) {
+		try {
+			List<Candidato> candidati = eventoService.visualizzaCandidati(evento);
+			ObservableList<Candidato> candidatiData = FXCollections.observableArrayList(candidati);
+			candidatiGrid   ??????? .setItems(candidatiData);
+		} catch (BusinessException e) {
+			dispatcher.renderError(e);
+		}
+	}
+
+	@FXML
+	public void indietroAction(ActionEvent event) {
+		dispatcher.renderView("homeElettore", elettore);
+	}
+}
